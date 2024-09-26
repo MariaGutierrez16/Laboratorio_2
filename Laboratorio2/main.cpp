3fe9e73cef0e3168a883a4154505adfeda6914c6
AlejandraG <maria.gutierrezr1@udea.edu.co>
2024-09-25 21:10:11 -0500

    25_09_24_1

--- Laboratorio2/main.cpp
+++ Laboratorio2/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-
+bool compararCadenas(const char* cadena1, const char* cadena2);
 
 int main() {
 
@@ -12,6 +14,8 @@ int main() {
     {
         cout<< "seleccione una opcion" <<endl;
         cout<< "1.Problema 1"<<endl;
+        cout<< "2.Problema 2"<<endl;
+        cout<< "3.Problema 3"<<endl;
         cout<< "0. Salir"<<endl;
         cin>>opcion;
 
@@ -60,6 +64,60 @@ int main() {
 
         }
         break;
+        case  2:{
+            cout<<"Ejecutando el problema 2 ..."<<endl;
+
+            srand(static_cast<unsigned int>(time(0)));  // Inicializa el generador de números aleatorios
+            const int tamano = 200;
+            char arreglo[tamano];
+            int contador[26] = {0};
+
+            // Generar el arreglo de letras aleatorias
+            for (int i = 0; i < tamano; ++i) {
+                arreglo[i] = 'A' + (rand() % 26);  // Genera una letra mayúscula aleatoria
+            }
+
+            // Imprimir el arreglo
+            for (int i = 0; i < tamano; ++i) {
+                std::cout << arreglo[i];
+            }
+            std::cout << std::endl;
+
+            // Contar cuántas veces se repite cada letra
+            for (int i = 0; i < tamano; ++i) {
+                contador[arreglo[i] - 'A']++;  // Incrementa el contador correspondiente a la letra
+            }
+
+            // Imprimir el conteo de cada letra
+            for (int i = 0; i < 26; ++i) {
+                if (contador[i] > 0) {
+                    std::cout << static_cast<char>('A' + i) << ": " << contador[i] << std::endl;
+                }
+            }
+
+        }
+           break;
+        case 3:{
+            cout<<"Ejecutando el problema 3 ..."<<endl;
+            const char cadena1[] = "hola";
+            const char cadena2[] = "hola";
+            const char cadena3[] = "mundo";
+
+            if (compararCadenas(cadena1, cadena2)) {
+                std::cout << "cadena1 y cadena2 son iguales." << std::endl;
+            } else {
+                std::cout << "cadena1 y cadena2 son diferentes." << std::endl;
+            }
+
+            if (compararCadenas(cadena1, cadena3)) {
+                std::cout << "cadena1 y cadena3 son iguales." << std::endl;
+            } else {
+                std::cout << "cadena1 y cadena3 son diferentes." << std::endl;
+            }
+
+        }
+        break;
+
         default:
             if(opcion!=0)
                 cout<<"opcion no valida"<<endl;
@@ -69,4 +127,34 @@ int main() {
 
     }
 
+}
+
+bool compararCadenas(const char* cadena1, const char* cadena2) {
+    // Comparar longitudes
+    int longitud1 = 0;
+    int longitud2 = 0;
+
+    // Calcular longitud de cadena1
+    while (cadena1[longitud1] != '\0') {
+        longitud1++;
+    }
+
+    // Calcular longitud de cadena2
+    while (cadena2[longitud2] != '\0') {
+        longitud2++;
+    }
+
+    // Si las longitudes son diferentes, las cadenas no son iguales
+    if (longitud1 != longitud2) {
+        return false;
+    }
+
+    // Comparar contenido
+    for (int i = 0; i < longitud1; ++i) {
+        if (cadena1[i] != cadena2[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- pruebas/main.cpp
+++ pruebas/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-using namespace std;
+int main() {
+    const int TAMANO = 200;
+    char arreglo[TAMANO];
+    int conteo[26] = {0};
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    srand(static_cast<unsigned int>(time(0))); // Inicializa la semilla para números aleatorios
+
+    // Generar el arreglo de letras mayúsculas aleatorias
+    for (int i = 0; i < TAMANO; ++i) {
+        arreglo[i] = 'A' + rand() % 26; // Genera una letra mayúscula aleatoria
+    }
+
+    // Imprimir el arreglo generado
+    for (int i = 0; i < TAMANO; ++i) {
+        std::cout << arreglo[i];
+    }
+    std::cout << std::endl;
+
+    // Contar cuántas veces se repite cada letra
+    for (int i = 0; i < TAMANO; ++i) {
+        conteo[arreglo[i] - 'A']++;
+    }
+
+    // Imprimir el conteo de cada letra
+    for (int i = 0; i < 26; ++i) {
+        if (conteo[i] > 0) {
+            std::cout << static_cast<char>('A' + i) << ": " << conteo[i] << std::endl;
+        }
+    }
+
     return 0;
 }
